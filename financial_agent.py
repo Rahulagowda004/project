5b744bd802a5ec4d48ef3546b1c80a99a05288c0
rahulagowda277 <rahulgowda277@gmail.com>
2025-01-02 15:38:04 +0530

    used google search instead of duckduckgo

--- financial_agent.py
+++ financial_agent.py
@@ -1,7 +1,7 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
-from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.googlesearch import GoogleSearch
 from dotenv import load_dotenv
 import os
 
@@ -12,29 +12,32 @@ os.environ["PHI_API_KEY"] = os.getenv("PHI_API_KEY")
 
 web_search_agent = Agent(
     name = "web search agent",
-    role = "Search the web for the information",
+    role = "Search the web for the information. Do not ask follow up questions",
     model = Groq(id = "llama3-groq-70b-8192-tool-use-preview",api_key=groq_api_key),
-    tools = [DuckDuckGo()],
-    instructions = ["always include sources"],
+    tools = [GoogleSearch()],
+    instructions = ["always include sources",
+                    "Given a topic by the user, respond with 4 latest news items about that topic.",
+        ],
     show_tool_calls=True,
     markdown = True
 )
 
 finance_agent = Agent(
     name="financial agent",
+    role = "You are trader who has meticulous knowledge of the stock market. You are responsible for providing the user with the financial data of the stock.",
     model = Groq(id = "llama3-groq-70b-8192-tool-use-preview",api_key=groq_api_key),
     tools=[YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,company_news=True)],
-    instructions=["use tables to display the data"],
+    instructions=["use tables to display the data", "Analyze the stock using the information provided by the web search agent. Provide relevant financial data, trends, and actionable suggestions for managing the stock, without asking follow-up questions."],
     show_tool_calls = True,
-    markdown=True
+    markdown=True,
 )
 
 Multi_ai_agent = Agent(
     model = Groq(id = "llama3-groq-70b-8192-tool-use-preview",api_key=groq_api_key),
+    role = "based on data from web search agent and financial agent, provide the user with a comprehensive analysis of the stock. Do not ask follow up questions",
     team = [web_search_agent, finance_agent],
     instructions = ["always include sources"],
     show_tool_calls = True,
     markdown = True
 )
-
-Multi_ai_agent.print_response("summarize analyst recommendations for Tesla",stream=True)
+Multi_ai_agent.print_response("give me the details of titan company ltd stock",stream=True)
